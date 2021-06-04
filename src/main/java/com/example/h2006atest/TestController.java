8a9494598b99db8eff3dc57bf453006337786ec0
hancong0311 <2608717151@qq.com>
2021-06-04 10:43:16 +0800

    ddddd

--- src/main/java/com/example/h2006atest/TestController.java
+++ src/main/java/com/example/h2006atest/TestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @RequestMapping("test")
     public String test(){
         return "test--00000--1------";
